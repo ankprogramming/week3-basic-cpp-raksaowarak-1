4d73b71c20bcc39c5ccd9df9782c4f9448c7a38a
Unknown <lovechillyhot@hotmail.com>
2017-11-28 11:45:15 +0700

    แก้ไขข้อที่ 3

--- lab_3_3.cpp
+++ lab_3_3.cpp
@@ -3,6 +3,22 @@
 using namespace std;
 
 int main() {
-  
-  return 0;
+ const float pi = 3.14315926536;
+ float r;
+ float h=15;
+ float Area;
+ float Around;
+ float Cylinder ;
+
+ cout << "Please enter radius : ";
+ cin>> r;
+ Area = pi *r*r;
+ cout << "Area of Circle : " << Area <<endl;
+
+ Around = 2 *pi*r;
+ cout << "Around of Circle : " << Around <<endl;
+
+ Cylinder = pi *r*h;
+ cout << "Cylinder of Circle : " << Cylinder <<endl;
+return 0;
 }
