6006bab8969351c7c0d23685613addda5420f5f7
Unknown <lovechillyhot@hotmail.com>
2017-11-28 10:57:01 +0700

    แก้ไปแล้วครึ่งนึง

--- lab_3_2.cpp
+++ lab_3_2.cpp
@@ -5,11 +5,22 @@ using namespace std;
 int main() {
   // เขียนโปรแกรมเพิ่ม เพื่อหาขนาดตัวแปรที่กำหนด
    cout << "Size of char : " << sizeof(char) << endl;
-   cout << "Size of short signed int : " << sizeof(short signed int) << endl;
-   cout << "Size of short unsigned int : " << sizeof(short unsigned int) << endl;
+   cout << "Size of signed char : " << sizeof(signed char) << endl;
+   cout << "Size of unsigned char : " << sizeof(unsigned char) << endl;
+   cout << "Size of int : " << sizeof(int) << endl;
+   cout << "Size of signed int  : " << sizeof(signed int) << endl;
+   cout << "Size of unsigned int: " << sizeof(unsigned int) << endl;
+   cout << "Size of short int : " << sizeof(short int) << endl;
+   cout << "Size of signed short int : " << sizeof(signed short int) << endl;
+   cout << "Size of unsigned short int : " << sizeof(unsigned short int) << endl;
    cout << "Size of long int : " << sizeof(long int) << endl;
+   cout << "Size of signed long int : " << sizeof(signed long int) << endl;
+   cout << "Size of unsigned long int : " << sizeof(unsigned long int) << endl;
    cout << "Size of float : " << sizeof(float) << endl;
-
+   cout << "Size of double : " << sizeof(double) << endl;
+   cout << "Size of long double int : " << sizeof(long double) << endl;
+   cout << "Size of wchar_t : " << sizeof(wchar_t) << endl;
+   cout << "Size of bool : " << sizeof(bool) << endl;
 
    // เขียนโปรแกรมเพิ่มเพื่อหาช่วงของค่าที่เก็บได้
    cout << "Min of char : " << numeric_limits<char>::min() << endl;
